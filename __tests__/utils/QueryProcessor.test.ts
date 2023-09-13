2f673fffb52d91f7ea8e050132f5e098d5c079a8
Yerim Song <yerim1111@gmail.com>
2023-09-13 12:27:49 -0400

    Added andrew id query

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -17,4 +17,12 @@ describe("QueryProcessor", () => {
             "writer in the English language and the world's pre-eminent dramatist."
           ));
     });
+
+    test('should return andrew id', () => {
+        const query = "id";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "yerims"
+          ));
+    });
 });
